102bfb7124853eb092d196f3a50d82672f29c3e7
feportoa <feporto.a@gmail.com>
2023-08-20 17:46:58 -0300

    Polyalph organized in POC_encrypt

--- POC_encrypt.cpp
+++ POC_encrypt.cpp
@@ -4,16 +4,13 @@
 #define ALPHABET "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 std::vector<std::string> Code(std::string decKey);
 
-int main() {
-    std::string str {"test"};
-    
+int main() 
+{    
+    std::string str;
     std::cin >> str;
-    
-    size_t find = str.find('b');
-    std::cout << find;
     std::vector<std::string> strVector = Code(str);
     for(size_t i {0}; i < strVector.size(); i++){
-        //std::cout << strVector[i] << std::endl;
+        std::cout << strVector[i] << std::endl;
     }
     return 0;
 }
@@ -23,8 +20,10 @@ std::vector<std::string> Code(std::string decKey)
     std::vector<std::string> alphabetKeys;
     for(size_t i {0}; i < decKey.size(); i++){
         size_t found = std::string(ALPHABET).find(decKey[i]);
-        alphabetKeys.push_back(std::string(ALPHABET).substr(0, found));
+        alphabetKeys.push_back(std::string(ALPHABET).substr(found+1));
+        alphabetKeys.at(i) += std::string(ALPHABET).substr(0, found+1);
         std::cout << std::string(ALPHABET).substr(0, found) << " | " << i << std::endl;
     }
+    
     return alphabetKeys;
 }
