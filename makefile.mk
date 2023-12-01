c58c61ab74718e266a481ae47e8ce85a3c3452f6
Fernando Gabriel Porto de Azevedo <feporto.a@gmail.com>
2023-11-30 22:32:14 -0300

    Added makefile

---
+++ makefile.mk
@@ -0,0 +1,12 @@
+SHELL = /usr/bin/env bash
+.PHONY: clean
+
+CC = g++
+CCFLAGS = -Wall -Werror -Wextra -std=c++20 -g -O
+CCAFTER = -lncurses
+
+main: main.o
+	$(CC) $(CCFLAGS) -o main main.o $(CCAFTER)
+
+clean:
+	rm -f main *.o
